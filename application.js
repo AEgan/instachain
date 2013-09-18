62af24b2b7dbfaff8eeb4ac7490570646f4d29e7
Alex Egan <aegan@andrew.cmu.edu>
2013-09-18 16:39:57 -0400

    added comments

--- application.js
+++ application.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
 	function instagood() {
+		// ajax call
 		$.ajax({
 			type: "GET",
 			dataType: "jsonp",
 			cache: false,
 			url: "https://api.instagram.com/v1/tags/"+$("#playTag").val()+"/media/recent?client_id=12798879df08427ca9115a9a331c2140",
 			success: function(response) {
+				// for testing purposes, loging the respose helps determine how to work with the object
 			  console.log(response);
+			  //the selected image
 			  var selected = response.data[Math.floor(Math.random() * response.data.length)];
 			  console.log(selected.images.standard_resolution.url);
+			  //adding the photo
 			  $("#photos").html("<img src='"+selected.images.standard_resolution.url+"'><br />");
+
+			  //looping through and adding a UL of tags for the chosen picture
 			  var ulString = "<ul>";
 			  for(var i = 0; i < selected.tags.length; i++)
 			  {
