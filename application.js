984668918a39818c94a9e25fc8c08c7c8d9898e6
Alex Egan <aegan@andrew.cmu.edu>
2013-09-18 21:28:52 -0400

    style

--- application.js
+++ application.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 			type: "GET",
 			dataType: "jsonp",
 			cache: false,
-			url: "https://api.instagram.com/v1/tags/"+$("#playTag").val()+"/media/recent?client_id=12798879df08427ca9115a9a331c2140",
+			url: "https://api.instagram.com/v1/tags/"+$("#playTag").val().replace(" ", "") +"/media/recent?client_id=12798879df08427ca9115a9a331c2140",
 			success: function(response) {
 				// for testing purposes, loging the respose helps determine how to work with the object
 			  console.log(response);
@@ -19,11 +19,12 @@ $(document).ready(function() {
 			  var ulString = "<ul>";
 			  for(var i = 0; i < selected.tags.length; i++)
 			  {
-			  	ulString += "<li>" + selected.tags[i] + "</li>";
+			  	ulString += "<li class='btn btn-primary'>" + selected.tags[i] + "</li>";
 			  }
+			  $("#intro").html("Showing a random result for " +$("#playTag").val());
 			  ulString += "</ul>"
-			  $("#photos").append("<br /><br />" + ulString);
-			  $("#photos").find("li").on("click", function() {
+			  $("#tags").html("<p>Tags for this image include:</p><br /><br />" + ulString);
+			  $("#tags").find("li").on("click", function() {
 			  	console.log($(this).text());
 			  	$("#playTag").val($(this).text());
 			  	$("#f1").submit();
